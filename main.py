9732636df45788975c4b9f2edb07df41b3f08d58
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-11-28 11:17:31 +0530

    implemeneted getting Http request without decoding

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ Host = '127.0.0.1'
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-server_socket.setblocking(False)
 
 server_socket.bind((Host,Port))
 server_socket.listen(5)
 print(f"Server is listening on {Host}:{Port}")
 
 while True :
-    try:
-        client_socket,client_address = server_socket.accept()
-        print(f"Client_socket {client_socket} : Client_ADdress{client_address}")
-    except:
-        time.sleep(2)
-        continue
+    client_socket,client_address = server_socket.accept()
+    client = client_socket.recv(1024)
+    print(client)
+
